f10234f60c37e2e6c5cd3fcd22162319a28c8d37
Jote Lijalem Gutema <jtljlm3@gmail.com>
2022-08-19 20:29:59 +0300

    update admin.py

--- core/admin.py
+++ core/admin.py
@@ -4,4 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Institution)
 
-#admin.site.register(User)
+admin.site.register(User)
+admin.site.register(ProfessionalProfile)
